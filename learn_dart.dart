a2ab90c9ca7ec94c112475ec27327fc96b29ba10
AVALON006 <avalon006@163.com>
2022-03-31 08:31:41 +0000

    变量的内容（切换到一个文件的版本）

--- learn_dart.dart
+++ learn_dart.dart
@@ -5,6 +5,20 @@ void helloWorld(){
   print('Hello World!');
 }
 
+var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter','Saturn','Uranus','Neptune'];//列表
+  var image = {
+  'tags':['saturn'],
+  'url':'//path/to/saturn.jpg'
+  };//字典
+void variables(){
+  // 变量 https://dart.cn/samples#variables
+  print('name is $name, year is $year, antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects, image is $image');
+}
+
 void main(){  
   helloWorld();
+  variables();
 }
