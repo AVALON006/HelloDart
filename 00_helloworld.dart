b14d6b6ddd42c15c4ce53101e839db91826051c7
AVALON006 <avalon006@163.com>
2022-03-31 08:30:02 +0000

    Hello World程序（切换到一个文件的版本）

--- 00_helloworld.dart
+++
@@ -1,4 +0,0 @@
-// 你好,世界! https://dart.cn/samples#hello-world
-void main(){
-  print('Hello World!');
-}
---
+++ learn_dart.dart
@@ -0,0 +1,10 @@
+// Learn Dart https://dart.cn/samples
+
+void helloWorld(){
+  // 你好,世界! https://dart.cn/samples#hello-world
+  print('Hello World!');
+}
+
+void main(){  
+  helloWorld();
+}
